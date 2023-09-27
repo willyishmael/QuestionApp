0579d391885ad5248bb12db05107a45927a76598
willyishmael <wtangka22@gmail.com>
2023-09-27 19:13:12 +0800

    Update question_detail.dart

--- lib/ui/screens/question_detail.dart
+++ lib/ui/screens/question_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_f2/data/model/question.dart';
+import 'package:task_f2/ui/widgets/question_widget.dart';
 
 class QuestionDetail extends StatefulWidget {
   final Question question;
@@ -14,13 +15,71 @@ class QuestionDetail extends StatefulWidget {
 }
 
 class _QuestionDetailState extends State<QuestionDetail> {
+  var hintIndex = -1;
+  var labelText = '';
+  var bodyText = '';
+  var buttonText = 'Show Hint';
+  var showButton = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Question Detail'),
       ),
-      body: Center(child: Text(widget.question.question)) // how can i access question from here?
+      body: Column(
+        children: [
+          QuestionWidget(
+            question: widget.question,
+            isPortrait: true,
+            onQuestionPressed: () {},
+          ),
+          Container(
+            padding: const EdgeInsets.all(12),
+            margin: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 201, 210, 236),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (labelText.isNotEmpty)
+                Text(
+                  labelText,
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+                if (bodyText.isNotEmpty)
+                Text(
+                  bodyText,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                if (showButton)
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    child: TextButton(
+                      onPressed: _nextButtonPressed,
+                      child: Text(buttonText),
+                    ),
+                  )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  void _nextButtonPressed() => setState(() {
+        if (hintIndex < widget.question.hints.length) {
+          hintIndex++;
+          labelText = 'HINT';
+          bodyText = widget.question.hints[hintIndex];
+          buttonText = 'Next Hint';
+        } else {
+          labelText = 'ANSWER';
+          bodyText = widget.question.answer;
+          showButton = false;
+        }
+      });
 }
